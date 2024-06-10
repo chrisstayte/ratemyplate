f3fc08735ebd9ffb84b1f885101592008a175a75
Chris <ctstayte@gmaill.com>
2024-06-09 20:31:04 -0500

    feat: Add recent entries section with dynamic data
    This commit adds functionality to fetch recent plates from the database and display them in a grid layout. The plates are sorted by timestamp in descending order and limited to 10 entries. Each plate is displayed as a card with its state and plate number.

--- src/components/recent-entries/recent-entries-section.tsx
+++ src/components/recent-entries/recent-entries-section.tsx
@@ -8,17 +8,42 @@ import {
 } from '@/components/ui/carousel';
 import * as React from 'react';
 import { auth } from '@/auth';
+import { database } from '@/db/database';
+import { Badge } from '@/components/ui/badge';
+import { desc, asc } from 'drizzle-orm';
+import { plates } from '@/db/schema';
 
 export default async function RecentEntriesSection() {
   const session = await auth();
 
-  if (!session) {
-    return <div>Not authenticated</div>;
-  }
+  // if (!session?.user) {
+  //   return <div>Not authenticated</div>;
+  // }
+
+  const recentPlates = await database.query.plates.findMany({
+    orderBy: [desc(plates.timestamp)],
+    limit: 10,
+  });
 
   return (
-    <div className='min-h-72  flex flex-col gap-10 items-center justify-center'>
+    <div className='flex flex-col gap-10 justify-center container '>
       <p className='text-2xl'>Recent reviews</p>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5'>
+        {recentPlates.map((review) => (
+          <Card
+            key={review.id}
+            className='aspect-video flex flex-col justify-center items-center'>
+            <div className='flex flex-col h-full relative'>
+              <div className='self-start'>
+                <Badge className='bg-blue-500 text-white'>{review.state}</Badge>
+              </div>
+              <div className='absolute inset-0 flex items-center justify-center'>
+                <p className='text-xl'>{review.plateNumber}</p>
+              </div>
+            </div>
+          </Card>
+        ))}
+      </div>
       {/* <Carousel className='w-full max-w-sm'>
         <CarouselContent className='-ml-1'>
           {Array.from({ length: 10 }).map((_, index) => (
